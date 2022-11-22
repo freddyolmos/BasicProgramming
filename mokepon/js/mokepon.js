502cfec5737b55a559a0caa8c93ba5f1f0fb844f
Fidel Olmos <fidelolmos334@gmail.com>
2022-11-22 17:22:17 -0600

    Se creo la carpeta js junto con mokepon.js

---
+++ mokepon/js/mokepon.js
@@ -0,0 +1,6 @@
+function seleccionarMascotaJugador(){
+    alert('Has elegido tu mascota')
+}
+
+let botonMascotaJugador=document.getElementById('boton-mascota')
+botonMascotaJugador.addEventListener('click',seleccionarMascotaJugador)
