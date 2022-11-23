059abd818b3fe5177ad3a844855c03933a975414
Fidel Olmos <fidelolmos334@gmail.com>
2022-11-23 13:26:53 -0600

    Aparece la mascota del jugadar seleccionada, y tambien se elige la mascota del enemigo con aleatoriedad

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -7,11 +7,27 @@ function seleccionarMascotaJugador(){
     let inputHipodoge=document.getElementById('hipodoge')
     let inputCapipepo=document.getElementById('capipepo')
     let inputRatigueya=document.getElementById('ratigueya')
+    let spanMascotaJugador=document.getElementById('mascota-jugador')
     
-    if(inputHipodoge.checked) alert('Seleccionaste a Hipodoge')
-    else if(inputCapipepo.checked) alert('Seleccionaste a Capipepo')
-    else if(inputRatigueya.checked) alert('Seleccionaste a Ratigueya')
+    if(inputHipodoge.checked) spanMascotaJugador.innerHTML='Hipodoge'
+    else if(inputCapipepo.checked) spanMascotaJugador.innerHTML='Capipepo'
+    else if(inputRatigueya.checked) spanMascotaJugador.innerHTML='Ratigueya'
     else alert('Selecciona una mascota')
+
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo(){
+    let ataqueAleatorio= aleatorio(1,3)
+    let spanMascotaEnemgo=document.getElementById('mascota-enemigo')
+
+    if(ataqueAleatorio==1) spanMascotaEnemgo.innerHTML='Hipodoge'
+    else if(ataqueAleatorio==2) spanMascotaEnemgo.innerHTML='Capipepo'
+    else spanMascotaEnemgo.innerHTML='Ratigueya'
+}
+
+function aleatorio(min,max){
+    return Math.floor(Math.random()*(max-min+1)+1)
 }
 
 window.addEventListener('load',iniciarJuego)
